8711e4f0f7de336bebb3231f3ecb80a653b9b6d0
GuilleP2018 <gpereyo@gmail.com>
2023-10-30 14:11:55 -0400

    Str uuid, mistake for __str__ func

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,13 @@ class BaseModel:
 
     def __init__(self):
         """initialize variables"""
-        self.id = uuid.uuid4
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """print a string"""
-        return f"[{self.__class__.__name__}] (self.id) (self.__dict__)"
+        return f"[{self.__class__.__name__}] ({self.id}) ({self.__dict__})"
 
     def save(self):
         """Saves info"""
