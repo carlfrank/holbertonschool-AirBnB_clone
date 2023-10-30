c2ae1f88b5641649d6a3409aee207634a17cb4e2
carlfrank <carl-frank7@hotmail.com>
2023-10-30 15:10:23 -0400

    updating args and kwargs in the init

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,30 @@ from datetime import datetime
 class BaseModel:
     """BaseModels class"""
 
-    def __init__(self):
-        """initialize variables"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModels instance"""
+        if kwargs:
+            # For each key in the dictionary, set the attribute with the associated value.
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                # Convert string datetime to datetime object.
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+                # Check if 'id' is not in kwargs, then assign it.
+            if "id" not in kwargs.keys():
+                self.id = str(uuid.uuid4())
+            # Check if 'created_at' is not in kwargs, then assign it.
+            if "created_at" not in kwargs.keys():
+                self.created_at = datetime.now()
+            # Check if 'updated_at' is not in kwargs, then assign it
+            if "updated_at" not in kwargs.keys():
+                self.updated_at = datetime.now()
+        else:
+            """initialize variables"""
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """print a string"""
